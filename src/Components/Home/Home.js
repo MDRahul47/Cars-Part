5274fdd3303aafa8b4a76a4d33869c4c23afa094
MDRahul47e <rahulahames@gmail.comm>
2022-05-22 13:51:35 +0600

    banner added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ import './Home.css';
 const Home = () => {
     return (
         <div>
-            {/* <Banner></Banner> */}
+            <Banner></Banner>
             {/* <BussinSummery></BussinSummery> */}
         </div>
     );
